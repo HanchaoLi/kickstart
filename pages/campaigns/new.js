737341e245deb531c698648872123164eea34722
HanchaoLi <seraphme1234@gmail.com>
2018-07-20 10:53:24 -0400

    set static routing part

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,7 +3,7 @@ import Layout from '../../components/Layout';
 import {Form, Button, Input, Message} from 'semantic-ui-react';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
-
+import {Router, Link} from '../../routes';
 class CampaignNew extends Component {
   state = {
     minimumContribution: '',
@@ -19,6 +19,7 @@ class CampaignNew extends Component {
       await factory.methods.createCampaign(this.state.minimumContribution).send({
         from : accounts[0]
       });
+      Router.pushRoute('/');
     } catch (err) {
       this.setState({ errorMessage : err.message});
     }
